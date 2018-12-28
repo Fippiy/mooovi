4fa0fc283dab25e5477656f3fa0fcaab9fdefa4e
Fippiy <profier4@gmail.com>
2018-12-28 19:40:35 +0900

    Edit migration file

--- db/migrate/20181228103801_create_products.rb
+++ db/migrate/20181228103801_create_products.rb
@@ -1,7 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string  :title
+      t.text    :image_url
+      t.string  :director
+      t.text    :detail
+      t.string  :open_date
       t.timestamps
     end
   end
